41e3e91105a7ebea3310162c1d86d4034f550796
Nazar <nazar.kolos90@gmail.com>
2020-03-11 22:27:46 +0200

    working in style criate todo list

---
+++ src/components/seach-panel.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './seach-panel.css';
+
+const SearchPenal = () => {
+    const searchText = 'Type here to search'
+    return <input placeholder={searchText} />;
+};
+
+export default SearchPenal;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const el = <h1>hillo world</h1>;
 
-ReactDom.render(el,
-     document.getElementById('root'));
+import AppHeader from './components/app-header';
+import TodoList from './components/todo-list';
+import SearchPanel from './components/seach-panel';
+import ItemStatusFilter from './components/item-status-filter';
+import './index.css';
+
+
+const App = () => {
+
+  const todoData = [
+    { label: 'Drink Coffee', important: false, id: 1 },
+    { label: 'Make Awesome App', important: true, id: 2 },
+    { label: 'Have a lunch', important: false, id: 3 }
+  ];
+
+  return (
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+
+      <TodoList todos={todoData} />
+    </div>
+  );
+};
+
+
+ReactDom.render(< App />,
+  document.getElementById('root'));
+
