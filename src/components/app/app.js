4f7a294e6461c2b292065e54dfd9d86b79a5729d
Nazar <nazar.kolos90@gmail.com>
2020-03-17 22:42:28 +0200

    working +- add elrment by logic

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component }  from 'react';
 
 import AppHeader from '../app-header/app-header';
 import TodoList from '../todo-list/todo-list';
@@ -6,26 +6,51 @@ import SearchPanel from '../search-panel/seach-panel';
 import ItemStatusFilter from '../item-status-filter/item-status-filter';
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
+  
+  state = {
+     todoData: [
+      { label: 'Drink Coffee', important: false, id: 1 },
+      { label: 'Make Awesome App', important: true, id: 2 },
+      { label: 'Have a lunch', important: false, id: 3 }
+    ]
+  };
 
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
-  ];
+  deleteItem = (id) => {
+    this.setState(( {todoData} ) => {
+      const idx = todoData.findIndex((el => el.id === id));
+      todoData.splice(idx, 1);
+      const before = todoData.slise(0, idx);
+      const after = todoData.slise(idx + 1);
+      const newArray = [
+        ...before,
+         ...after
+      ];
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+      return {
+        todoData: newArray
+      };
+    });
+  };
+  
+  render(){
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+  
+        <TodoList 
+        todos={this.state.todoData} 
+        onDeleted = { this.deleteItem }/>
       </div>
+    );
+  }
 
-      <TodoList todos={todoData} />
-    </div>
-  );
+  
 };
 
-export default App;
+
 
--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -3,12 +3,14 @@ import React from 'react';
 import TodoListItem from '../todo-list-item/todo-list-item';
 import './todo-list.css';
 
-const Todolist = ({ todos }) => {
+const Todolist = ({ todos, onDeleted }) => {
   const elements = todos.map((item) => {
     const { id, ...itemProps } = item;
     return (
       <li key={id} className="list-group-item" >
-        <TodoListItem  {...itemProps} />
+        <TodoListItem
+          {...itemProps}
+          onDeleted={() => onDeleted(id)} />
       </li>
     );
   });
